349762894906a7ec266112e4bd5892a4fc87e0d9
Fatima-web06 <fatima.fall@epitech.digital>
2024-09-30 12:06:53 +0200

    Create  ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,7 @@
+const firstname = "Jonh"
+const lastname = "Doe"
+const cardCode = "1111 2222 3333 4444"
+const expiryDateMonth = 21
+const expiryDateYear = 2142
+let computedBankInfo =  firstname+ "," +lastname+ "," +cardCode+ "," +expiryDateMonth+ "/" +expiryDateYear;
+displayInCard (computedBankInfo);
