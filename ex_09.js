f2faa221f0b3a16bc10dfb6f1593da3e0dd0e30c
Fatima-web06 <fatima.fall@epitech.digital>
2024-09-30 15:14:30 +0200

    Create ex_09.js

---
+++ ex_09.js
@@ -0,0 +1,11 @@
+displayThisText ("The variable value is " + imput_var)
+if (Number.isInteger(imput_var)) {
+    displayThisText("Its type is integer")
+} else if (Number.isFinite(imput_var)) {
+   displayThisText("Its type is float");
+   } else { 
+    displayThisText("Its type is" + typeof imput_var)
+    }
+if (imput_var == 42) {
+    displayThisText("Its is the meaning of life");
+}
