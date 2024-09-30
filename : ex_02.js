2621df42b3231c29f8da4d6d93ea11934f3a7159
Fatima-web06 <fatima.fall@epitech.digital>
2024-09-30 10:38:16 +0200

    Create : ex_02.js

---
+++ : ex_02.js
@@ -0,0 +1,10 @@
+let integer = "42"
+displayThisText(integer)
+let float = "42,42"
+displayThisText(float)
+const string = "forty two"
+displayThisText(string)
+let bool = "true"
+displayThisText(bool)
+const this_is_null = "nul"
+displayThisText(this_is_nul)
