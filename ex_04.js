eeac574b644ec1cd43e2d94a98b3fab0afc44139
Fatima-web06 <fatima.fall@epitech.digital>
2024-09-30 11:27:58 +0200

    Create  ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof x);
+displayThisText(typeof false);
+displayThisText(typeof 33);
+displayThisText(typeof 1234n);
+displayThisText(typeof Symbol());
+displayThisText(typeof null);
+displayThisText(typeof displayThisText);
